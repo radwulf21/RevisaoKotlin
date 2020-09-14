f2498cfa111b646fa4394bb3be00df310c6e0633
maraisi <maraisidepaula@gmail.com>
2020-09-14 17:03:52 -0300

    classe aluno

---
+++ src/exc1/Aluno.kt
@@ -0,0 +1,34 @@
+package exc1
+
+class Aluno (val nome:String, val sobrenome: String = "Doe", val registroAcademico: String = "007"){
+    fun assistirAula(aula:Aula) {
+        println("Assistindo aula $aula")
+    }
+
+    fun fazerLicao() {
+        println("Fazendo lição")
+    }
+
+    override fun toString() = "$nome $sobrenome (ra: $registroAcadmeico)"
+}
+
+
+//class Aluno (val nome: String, val sobrenome: String, val registroAcademico: String) {
+//
+//    fun assitirAula (var presenca: Boolean): String {
+//
+//        when(presenca){
+//            true -> println("Presente")
+//            false -> println("Ausente")
+//        }
+//    }
+
+//    fun fazerLicoes(var paraCasaFeito: Boolean): String {
+//        if(assistirAula){
+//            when(paraCasaFeito){
+//                true -> println("Feito")
+//                false -> println("Que feio, não fez o para casa!")
+//            }
+//        }
+//    }
+//}
